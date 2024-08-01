abd2c0b0242acef0486ab816cc1c47367cba1d19
SpairowMichael <evv79767@gmail.com>
2024-08-01 20:33:52 +0300

    program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random binary code");
+            Console.Write("\nHow many numbers you need:");
+            int numberLength = Convert.ToInt32(Console.ReadLine()); 
+
+            int[] cucumbers = new int[numberLength];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < cucumbers.Length; i++)
+            {
+                cucumbers[i] = rand.Next(0,2);
+
+                Console.Write($"{cucumbers[i]}");
+            }
+        }
+    }
+                    
+}
